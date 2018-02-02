4e572090b1ff5875743bb6364e4ed8eb26ba06e3
Ahsan <ahsan.tanweer.26@gmail.com>
2018-02-02 10:56:58 +0500

    Multi-Testing per category

--- utils/mapper.py
+++ utils/mapper.py
@@ -36,51 +36,54 @@ did_login = False
 failed_action = []
 passed = 0
 failed = 0
+user_category = ['agent', 'admin', 'buyer']
 
 test_actions, expected_result = parse_language()
 print('======== STARTING TEST ========\n')
 for index, test_action in enumerate(test_actions):
-    print('\n')
-    last_action = 'Success'
-    print('======== TEST:', index + 1, ' ========')
-    print("Expected Result: ", expected_result[index])
-    print('-------------------------------')
-    for action in test_action:
-        if last_action != "Success":
-            break
-        function = action
-        if '-' in action:
-            param = action.split('-')[-1]
-            function = action.split('-')[0] + '-'
-            print('Function called: ', action_step[function])
-            print('Parameters:', param)
-            last_action = action_step[function](param)
+    for category in user_category:
+        print('======== Category :', category, ' ========')
+        print('\n')
+        last_action = 'Success'
+        print('======== TEST:', index + 1, ' ========')
+        print("Expected Result: ", expected_result[index])
+        print('-------------------------------')
+        for action in test_action:
+            if last_action != "Success":
+                break
+            function = action
+            if '-' in action:
+                param = action.split('-')[-1]
+                function = action.split('-')[0] + '-'
+                print('Function called: ', action_step[function])
+                print('Parameters:', param)
+                last_action = action_step[function](param)
+            else:
+                if('login' in action):
+                    did_login = True
+                print('Function called: ', action_step[function](category))
+                last_action = action_step[function]()
+
+            print('Status: ', last_action)
+
+        # To logout after every test
+        if did_login:
+            did_login = False
+            print('Function called: logout')
+            last_action_logout = action_step['logout']()
+            print('Status: ', last_action_logout)
+
+        if last_action == 'Success':
+            passed = passed + 1
+            print("----------------")
+            print("TEST SUCCESSFUL")
+            print("----------------")
         else:
-            if('login' in action):
-                did_login = True
-            print('Function called: ', action_step[function])
-            last_action = action_step[function]()
-
-        print('Status: ', last_action)
-
-    # To logout after every test
-    if did_login:
-        did_login = False
-        print('Function called: logout')
-        last_action_logout = action_step['logout']()
-        print('Status: ', last_action_logout)
-
-    if last_action == 'Success':
-        passed = passed + 1
-        print("----------------")
-        print("TEST SUCCESSFUL")
-        print("----------------")
-    else:
-        failed = failed + 1
-        failed_action.append(function + ' ' + param)
-        print("----------------")
-        print("TEST FAILED")
-        print("----------------")
+            failed = failed + 1
+            failed_action.append(function + ' ' + param)
+            print("----------------")
+            print("TEST FAILED")
+            print("----------------")
 
 
 print("=================")
