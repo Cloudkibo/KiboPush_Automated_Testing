94434eb2040a48ff44520833cb83f9efb466d817
Muzamil Bukhari <muzamil.bukhari.39@gmail.com>
2019-08-06 17:09:32 +0500

    add_server_testcases_broadcast

---
+++ Server_Api_testCases/Object Repository/Broadcast_api/Link Carousal/sendBroadcast_With_OneLink.rs
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>sendBroadcast_With_OneLink</name>
+   <tag></tag>
+   <elementGuidId>215e122e-6c6d-4e51-bbff-1babe496eacf</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n\&quot;fbMessageTag\&quot;: \&quot;NON_PROMOTIONAL_SUBSCRIPTION\&quot;,\n\&quot;isList\&quot;: false,\n\&quot;isSegmented\&quot;: true,\n\&quot;payload\&quot;: [\n {\n\t\&quot;buttons\&quot;: [{ \n     \t\&quot;title\&quot;: \&quot;Go to Article\&quot;,\n   \t\t\&quot;type\&quot;: \&quot;web_url\&quot;,\n         \&quot;url\&quot;: \&quot;https://images.dawn.com/news/1183430\&quot;\n  }          \n],\n\t\&quot;componentType\&quot;: \&quot;card\&quot;,\n    \&quot;description\&quot;: \&quot;\u0027Have we conveniently blocked what we don’t want to address? This is beyond line...\&quot;,\n\t\&quot;fileName\&quot;: \&quot;\&quot;,\n   \t\&quot;fileurl\&quot;: \&quot;\&quot;,\n   \t\&quot;id\&quot;: 1564996227836,           \&quot;image_url\&quot;:\&quot;https://i.dawn.com/large/2019/08/5d47e761c19fe.jpg\&quot;,\n        \&quot;links\&quot;:[{\n     \&quot;loading\&quot;: false,\n\t \&quot;url\&quot;: \&quot;https://images.dawn.com/news/1183430\&quot;,\n\t \&quot;valid\&quot;: true\n \n }],\n   \t\&quot;size\&quot;: \&quot;\&quot;,\n\t\&quot;title\&quot;:\&quot;Pakistani celebrities stand up against human rights violations in Kashmir\&quot;,\n   \t\&quot;type\&quot;: \&quot;\&quot;\n\n\n }\n],\n\&quot;platform\&quot;: \&quot;facebook\&quot;,\n\&quot;segmentationGender\&quot;: [],\n\&quot;segmentationList\&quot;: [],\n\&quot;segmentationLocale\&quot;: [],\n\&quot;segmentationPageIds\&quot;: [\&quot;5c0902321276ef481a715918\&quot;],\n\&quot;segmentationTags\&quot;: [],\n\&quot;segmentationTimeZone\&quot;: \&quot;\&quot;,\n\&quot;subscribersCount\&quot;: 6,\n\&quot;title\&quot;: \&quot;Broadcast Title\&quot;\n}\n\n&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzA5MDE5ZDEyNzZlZjQ4MWE3MTU5MDkiLCJpYXQiOjE1NjQ5ODA0OTEsImV4cCI6MTU2NTMyNjA5MX0.4m0km4qma7If6YJVvxiBJVj2U77-fIQkoFm-Sv79Wc0</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://kiboengage.cloudkibo.com/api/broadcasts/sendConversation</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Server_Api_testCases/Object Repository/Broadcast_api/youtubeComponent/sendBroadcast_video _with_MultipleButton.rs
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>sendBroadcast_video _with_MultipleButton</name>
+   <tag></tag>
+   <elementGuidId>224e1380-1a45-4685-87b5-348e08d7fc3f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n\&quot;fbMessageTag\&quot;: \&quot;NON_PROMOTIONAL_SUBSCRIPTION\&quot;,\n\&quot;isList\&quot;: false,\n\&quot;isSegmented\&quot;: true,\n\&quot;payload\&quot;: [\n {\n\t\t\&quot;buttons\&quot;: [{ \n     \t\&quot;title\&quot;: \&quot;google\&quot;,\n   \t\t\&quot;type\&quot;: \&quot;web_url\&quot;,\n \&quot;url\&quot;:\&quot;https://kiboengage.cloudkibo.com/api/URL/broadcast/5d47b7709386c778c27ae4de\&quot;\n  } , \n {\&quot;title\&quot;: \&quot;facebook\&quot;,\n   \&quot;type\&quot;: \&quot;web_url\&quot;,\n \&quot;url\&quot;:\&quot;https://kiboengage.cloudkibo.com/api/URL/broadcast/5d47ba959386c778c27ae503\&quot;\n } , \n {\&quot;title\&quot;: \&quot;gmail\&quot;,\n   \&quot;type\&quot;: \&quot;web_url\&quot;,\n \&quot;url\&quot;:\&quot;https://kiboengage.cloudkibo.com/api/URL/broadcast/5d47ba959386c778c27ae504\&quot;\n }           \n],\n\t\&quot;componentType\&quot;: \&quot;media\&quot;,\n\t\&quot;fileName\&quot;: \&quot;_-rZkpCEDIGMo.mp4\&quot;,\n   \t\&quot;id\&quot;: 1564994762660,           \n    \&quot;image_url\&quot;:\&quot;\&quot;,\n\t\&quot;mediaType\&quot;: \&quot;video\&quot;,\n\t\&quot;size\&quot;: 3619223,\n\t\&quot;type\&quot;: \&quot;video/mp4\&quot;,\n    \&quot;videoLink\&quot;: \&quot;https://accounts.cloudkibo.com/api/v1/files/download/f055307b90e20198584514.mp4\&quot;,\n   \&quot;youtubeLink\&quot;: \&quot;https://www.youtube.com/watch?v\u003drZkpCEDIGMo\&quot;,\n\t\&quot;fileurl\&quot;: {\n  \t\t\&quot;attachment_id\&quot;: \&quot;483285482487291\&quot;,\n\t\t\&quot;id\&quot;: \&quot;f055307b90e20198584514.mp4\&quot;,\n\t\t\&quot;name\&quot;: \&quot;_-rZkpCEDIGMo.mp4\&quot;,\n\t\t\&quot;url\&quot;:\t\&quot;https://accounts.cloudkibo.com/api/v1/files/download/f055307b90e20198584514.mp4\&quot;\n}\n }\n],\n\&quot;platform\&quot;: \&quot;facebook\&quot;,\n\&quot;segmentationGender\&quot;: [],\n\&quot;segmentationList\&quot;: [],\n\&quot;segmentationLocale\&quot;: [],\n\&quot;segmentationPageIds\&quot;: [\&quot;5c0902321276ef481a715918\&quot;],\n\&quot;segmentationTags\&quot;: [],\n\&quot;segmentationTimeZone\&quot;: \&quot;\&quot;,\n\&quot;subscribersCount\&quot;: 6,\n\&quot;title\&quot;: \&quot;Broadcast Title\&quot;\n}\n\n&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzA5MDE5ZDEyNzZlZjQ4MWE3MTU5MDkiLCJpYXQiOjE1NjQ5ODA0OTEsImV4cCI6MTU2NTMyNjA5MX0.4m0km4qma7If6YJVvxiBJVj2U77-fIQkoFm-Sv79Wc0</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://kiboengage.cloudkibo.com/api/broadcasts/sendConversation</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
