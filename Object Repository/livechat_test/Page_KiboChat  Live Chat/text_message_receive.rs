081bdcb242a28e3348a5abd2a6009d7e6c83e50e
arveen kumar <arveenkumar55@gmail.com>
2019-11-08 12:43:36 +0500

    add_new_testcases

---
+++ Object Repository/livechat_test/Page_KiboChat  Live Chat/text_message_receive.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>text_message_receive</name>
+   <tag></tag>
+   <elementGuidId>2932cc97-b436-4177-b0aa-86ca7a43c5b6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;messageGroup&quot;]/div[3]/div/div[1]/div/div/div[1]/div/span/div/div/span</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/Live_chat_performance/new_test_1_from_messenger to kibopush/test_for_multiple_image/Script1573187332484.groovy
@@ -0,0 +1,62 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.By as By
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl('https://m.facebook.com/')
+
+WebUI.delay(2)
+
+WebUI.setText(findTestObject('Object Repository/messenger_UI/Page_Facebook  log in or sign up/input_facebook_email'), 'arveen.langhani@yahoo.com')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/messenger_UI/Page_Facebook  log in or sign up/input_facebook_pass'), 
+    'Sq3Tv/oKKfDNGi1QgSywNA==')
+
+WebUI.click(findTestObject('Object Repository/messenger_UI/Page_Facebook  log in or sign up/button_Log In'))
+
+
+WebUI.click(findTestObject('Object Repository/livechat_test/Page_Facebook/h3_Log In With One Tap'))
+
+WebUI.click(findTestObject('Object Repository/livechat_test/Page_Facebook/button_OK'))
+
+WebUI.delay(2)
+WebUI.click(findTestObject('Object Repository/livechat_test/Page_Facebook/div_2'))
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('Object Repository/livechat_test/Page_Messages/a_Education123'))
+
+WebUI.delay(5)
+
+
+WebDriver driver = DriverFactory.getWebDriver()
+
+WebElement upload = driver.findElement(By.xpath('//input[@type=\'file\']'))
+
+String image = RunConfiguration.getProjectDir() + '/sample.jpg'
+
+upload.sendKeys(image)
+
+WebUI.waitForElementHasAttribute(findTestObject('Object Repository/livechat_test/Page_KiboChat  Live Chat/text_message_receive'),'class', 200, FailureHandling.STOP_ON_FAILURE)
+WebUI.delay(5)
+WebUI.click(findTestObject('Object Repository/livechat_test/Page_Education123/button_Send'))
+WebUI.delay(2)
+
