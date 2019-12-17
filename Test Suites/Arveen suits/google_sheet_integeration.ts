5bd02131f7b0fd0fe2935eae0f9f60f21f64b97b
arveen kumar <arveenkumar55@gmail.com>
2019-12-17 16:51:47 +0500

    add_google_sheet_testcases

---
+++ Test Suites/Arveen suits/google_sheet_integeration.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>google_sheet_integeration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5beb8c86-158a-478c-994c-9a7c0f63c4a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>c9a9b499-8f23-4059-8f8e-8d6748fbf025</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/s-sidebar_navigation/individualAccount/arveen_production_login_kiboengage</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a67ee296-6f25-4ccb-b2e8-ad78ed2a89b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Broadcasts/open_broadcast</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>364b3706-cb1b-4e47-81ff-dc8c2888d1ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Broadcasts/google_sheet_integeration/TC0006</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
