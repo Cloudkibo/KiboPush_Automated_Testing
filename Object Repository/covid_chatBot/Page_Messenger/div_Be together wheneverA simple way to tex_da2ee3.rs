667401f2bcb16f769ff0dee5ba386b91fd40da0c
arveenkumar55 <arveen@cloudkibo.com>
2020-04-07 17:23:49 +0500

    covid_chat_bot_testcases

---
+++ Object Repository/covid_chatBot/Page_Messenger/div_Be together wheneverA simple way to tex_da2ee3.rs
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Be together wheneverA simple way to tex_da2ee3</name>
+   <tag></tag>
+   <elementGuidId>ce87710b-d34d-4d24-bb19-47a4d8c1ecd7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>_4wyl _4wyq _2k-y _2k-q _4wyu</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Be together, whenever.A simple way to text, video chat and plan things all in one place.Sign InKeep me signed inForgotten your password?</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;facebook&quot;)/body[@class=&quot;_605a chrome webkit x1 Locale_en_GB&quot;]/div[1]/div[@class=&quot;_6az_ _6b69 _2tqi _8mgu&quot;]/div[1]/div[@class=&quot;_4wyl _4wyq _2k-y _2k-q _4wyu&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//html[@id='facebook']/body/div/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='For developers'])[1]/following::div[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Privacy &amp; safety'])[1]/following::div[4]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Arveen suits/Covid_chatBot.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Covid_chatBot</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4bff0884-c985-48ee-9fb6-da73d6f8ba73</testSuiteGuid>
+   <testCaseLink>
+      <guid>a043e376-2557-4919-b6bf-86be17abc22e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5bc59043-a90b-4750-a37b-43cdbff79874</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9bc6f03-8f50-4afd-a0c9-8771477e1d1a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9cdd66e-4718-44d1-87d9-d223ed795576</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6a4b46e-49d2-4a8b-a213-a337680b458b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC005</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c08efaf5-6288-4a6a-b284-bf8fee6431d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC005_N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>77cb672a-ad02-459c-bbe1-a344753c881c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC006</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>64262cc0-16dc-45b8-bbbc-773cf89b8500</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC007</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>693fbbcf-352c-432c-86b7-cea310585fdc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC016</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>199803e4-881a-494f-abae-ac05370eaa6e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC017</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b34d651-6f7f-4e9e-a603-9346dfe0c523</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC018</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>344829d3-eb77-4890-a911-d7d683abe293</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC019</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b792e054-8e84-4a32-b9e0-09b734dbee29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC020</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7bf8c21-5096-4144-94ef-1db2e02c7df0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC021</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>62ac8a0a-c474-47a9-bc29-b03d18ce4774</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC022</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>759508dc-de50-476f-aa48-19548b5c9994</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC023</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4f1e4cc4-04f7-488b-9094-ef5bab337513</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC024</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c77d2d47-290e-49c2-b0ee-dd82445fd278</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC025</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2a2a3c4f-7fb3-437a-8c62-cd3af8ac4749</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC026</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ce338ec-211e-4017-8cbd-6213f7bd23c1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC027</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d16d9cae-12af-415a-b05c-7a01ad1e4a26</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC028</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a298b313-b01c-4cb6-9737-e75532ee37b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC029</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c36adad5-0ff3-4a8b-b5ca-cadd85e873b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC008</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a1158671-41a8-49fa-a215-06633ce94770</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC009</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cfb22d25-80c3-475a-9122-678f0c2b1945</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC010</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>41cbf773-3588-4b4c-b2d4-02cdaa6a4879</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC011</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2b168acd-ae5f-4ad0-b1ea-d7bb088e4888</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC012</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cbde0c15-209a-443c-9610-074008c854a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC013</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe3bd58f-8df3-4465-a514-20972df70457</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC014</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c21b4d19-e64c-4452-826c-330cf0645aeb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Covid_ChatBot/TC015</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
